66c5de2630b36a26be200af59131addea6118617
George <georgedzuricsko1@gmail.com>
2017-10-05 15:58:44 -0500

    updated schema and gemfile

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,20 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170922011228) do
+ActiveRecord::Schema.define(version: 20171003192506) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "conditions", force: :cascade do |t|
+    t.string "status"
+    t.integer "weather_report_id"
+    t.integer "author_id"
+    t.integer "park_id"
+    t.string "notes"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "parks", force: :cascade do |t|
     t.string "name"
